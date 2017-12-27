f70fb53ae913ce7b750d892efdbe3943ac03ba68
Odaryna <odaryna.korolevych@gmail.com>
2017-12-27 16:40:58 +0200

    Initial Commit

---
+++ Test task/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Test task
+//
+//  Created by Odaryna on 12/27/17.
+//  Copyright © 2017 Odaryna. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
